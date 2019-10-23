976fcae46b20e0eb990ff2d4481bfdb8b8338cfd
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2019-10-23 16:56:37 -0700

    core files updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { MovieDetailsComponent } from './movie-details/movie-details.component';
     NavbarComponent,
     SidebarComponent,
     FooterComponent,
-    MovieDetailsComponent
+    MovieDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
