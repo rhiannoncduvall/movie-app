fb8dea7322a9fd9b0bb87e43963729fb75ecc15b
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2019-10-23 16:57:38 -0700

    fixed spacing and size of cards and shortened movie descriptions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { APIDataService } from '../api-data.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,21 +10,23 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
   topRatedMovies: string = "/movie/top_rated";
   pageTitle: string;
+  maxWordCountForOverView: number = 50;
 
-  constructor(public movieService: APIDataService, private router: Router) { }
+  constructor(public movieService: APIDataService, private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit() {
-  }
 
   showTopRatedMovies(pageTitle: string) {
     this.movieService.displayMovies(this.topRatedMovies, pageTitle)
     }
 
+  navigateToMovieDetails(id) {
+    this.router.navigate(['/movie-details'])
+    // this.router.navigate(['../movie-details'], { relativeTo: this.route });
+  }
 
 
-  navigateToMovieDetails() {
-    // this.router.navigateByUrl('../movie-details');
-    this.router.navigate(['../movie-details'], { relativeTo: this.route });
+  ngOnInit() {
+    this.movieService.displayMovies(this.topRatedMovies, 'Top Rated');
   }
 
 
